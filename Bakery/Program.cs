c903fd55a3a136f9a854bd7007a3f0dc6a841c6f
Messquerade <annac.klingberg@gmail.com>
2021-09-24 15:52:46 -0700

    add BreadOrder() repeats if invalid user input

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -26,32 +26,47 @@ namespace Bakery
           {
             Bread userBreadOrder = new Bread(loavesNumber);
             return userBreadOrder.GetBreadCost();
-          } else if (loavesNumber < 0)
+          }  else if (loavesNumber > 100)
           {
             Console.WriteLine("To make an extra large order, please email pierre@pierrebakery.com");
             return -1;
           } else
           {
             Console.WriteLine("Please enter a positive whole number of how many loaves of bread you wish to buy (example: 0, 1, 15) or q to quit");
-            return -2;
+            return BreadOrder();
           }
         } else
         {
-          return -1;
+            Console.WriteLine("Please enter a positive whole number of how many loaves of bread you wish to buy (example: 0, 1, 15 etc.) or q to quit");
+            return BreadOrder();
         }
+      } else if (breadResponse == "n")
+      {
+        return 0;
       } else
       {
-        return -1;
+        Console.WriteLine("Please enter y for yes, n for no, or q to quit");
+        return BreadOrder();
       }
     }
+
+    // public static void CheckResponse(int breadTotalCost)
+    // {
+    //   if (breadTotalCost == -2)
+    //   {
+        
+    //   }
+    // }
 
     public static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery! We offer a fresh selection of bread and pastries baked in house daily. Enter q at anytime to quit.");
       Console.WriteLine("Bread costs $5.00 per loaf. Now offering our special deal: every third loaf is free!");
       Console.WriteLine("Pastries cost $2.00 each. Now offering our special deal: every third pastry is half off!");
-      int BreadCost = BreadOrder();
-      Console.WriteLine(BreadCost);
+      Console.WriteLine("For orders over 100 baked goods, please email pierre@pierrebakery.com");
+      int breadCost = BreadOrder();
+      Console.WriteLine(breadCost);
+     
 
 
     }
